2163d377274689c1d2889812585e374ca0d20b45
jirzy <87252@glr.nl>
2023-05-31 09:49:18 +0200

    updated front-end#2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,51 +1,38 @@
-
-<div class="grid grid-cols-2 ">
-    <div class="w-full">
-        <img class="h-screen object-cover" src="{{asset("storage/lighthouse.jpeg")}}" alt="">
-    </div>
-<x-guest-layout>
+<x-app-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-
-
-    <div>
-    <form method="POST" action="{{ route('login') }}">
+<div class="w-full h-screen flex items-center justify-center bg-gray-900">
+    <form method="POST" class="w-full h-full flex items-center justify-center flex-col" action="{{ route('login') }}">
         @csrf
+        <div class="w-1/5 h-3/5 ">
+        <h1 class="text-[#c5c5c5] text-5xl w-full text-center mb-4">Login</h1>
+            <div class="w-full h-fit">
+                <!-- Email Address -->
+                <div>
+                    <x-input-label class="text-[#C5C5C5] m-1" for="email" :value="__('Email')" />
+                    <x-text-input id="email" class="m-1 bg-[#00000020] w-full p-2" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+                <!-- Password -->
+                <div>
+                    <x-input-label class="text-[#C5C5C5] m-1" for="password" :value="__('Password')" />
+                    <x-text-input id="password" class="m-1 bg-[#00000020] w-full p-2"
+                                  type="password"
+                                  name="password"
+                                  required autocomplete="current-password" />
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+                <div class="flex items-center justify-end mt-4">
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+                    <x-primary-button class="ml-3">
+                        {{ __('Log in') }}
+                    </x-primary-button>
+                </div>
+            </div>
         </div>
     </form>
-    </div>
 </div>
-</x-guest-layout>
+</x-app-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,23 +55,38 @@
                 </div>
             </div>
     @else
-            <div class="navbar bg-gray-900 border-b border-[#393F4A] mb-4 sticky top-0">
-            <div class="navbar-start">
-                <div class="dropdown">
-                    <label tabindex="0" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a href="/">Home</a></li>
-                    </ul>
+            <div class="navbar bg-gray-900 border-b border-[#393F4A] mb-4 sticky top-0 z-30">
+                <div class="navbar-start lg:hidden">
+                    <div class="dropdown">
+                        <label tabindex="0" class="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/search">search</a></li>
+                            <li><a href="/crud">Add post</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <a class="btn btn-ghost normal-case text-xl">skiter</a>
-            </div>
-            <div class="navbar-center hidden lg:flex">
-                <ul class="menu menu-horizontal px-1">
-                    <li><a href="/">Home</a></li>
-                </ul>
-            </div>
+                <div class="navbar-start hidden lg:flex">
+                    <ul class="menu menu-horizontal px-1">
+                        <li><a href="/">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                                </svg>
+
+                            </a></li>
+                        <li><a href="/search">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                                </svg>
+                            </a></li>
+                    </ul>
+
+                </div>
+                <div class="navbar-center">
+                    <a class="normal-case text-xl">skiter</a>
+                </div>
             <div class="navbar-end">
                 <a href="/login" class="btn">login/register</a>
             </div>
@@ -103,22 +118,16 @@
         @endforeach
     </div>
     @else
-
-
-<div class="w-full text-center mt-10">
-    <p class="text-4xl">tweets</p>
-</div>
-        <hr>
-        <div class=" mt-4 grid grid-cols-3 gap-4">
-            @foreach($messages as $message)
-                <div class="card bg-base-100 shadow-xl">
-                    <div class="card-body text-center">
-                        <p class="text-3xl">{{ $message->titel  }}</p>
-                        <p>{{ $message->text }}</p>
+            <div class="flex flex-col items-center gap-14 pb-14">
+                @foreach($messages as $message)
+                    <div class="card w-2/4 bg-[url('https://images.unsplash.com/photo-1612441804231-77a36b284856?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')] bg-cover aspect-[16/9] shadow-xl rounded-xl">
+                        <h2 class="card-title text-2xl text-[#c5c5c5] h-14 pl-6 bg-gradient-to-b from-[#00000082] via-[#00000055] to-[#00000000] rounded-t-xl">{{ $message->titel  }}</h2>
+                        <div class="card-body group">
+                            <p class="opacity-0 group-hover:opacity-100 w-full h-full transition ease-in-out duration-500 text-[#c5c5c5]">{{ $message->text }} </p>
+                        </div>
                     </div>
-                </div>
-            @endforeach
-        </div>
+                @endforeach
+            </div>
     @endauth
     </div>
 

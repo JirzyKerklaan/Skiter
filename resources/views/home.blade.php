5337f270059b39f12fef68dfd10181a5c87e393c
jirzy <87252@glr.nl>
2023-06-07 11:12:55 +0200

    Like button update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -103,11 +103,26 @@
     <div class="flex flex-col items-center gap-14 pb-14">
         @foreach($messages as $message)
                 <div class="card w-2/4 bg-[url('https://images.unsplash.com/photo-1612441804231-77a36b284856?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')] bg-cover aspect-[16/9] shadow-xl rounded-xl">
-                    <h2 class="card-title text-2xl text-[#c5c5c5] h-14 pl-6 bg-gradient-to-b from-[#00000082] via-[#00000055] to-[#00000000] rounded-t-xl">{{ $message->titel  }}</h2>
-                    <div class="card-body group">
-                        <p class="opacity-0 group-hover:opacity-100 w-full h-full transition ease-in-out duration-500 text-[#c5c5c5]">{{ $message->text }} </p>
-                        <div class="card-actions justify-end items-center">
+                    <div class="flex flex-row bg-gradient-to-b from-[#00000082] via-[#00000055] to-[#00000000] rounded-t-xl">
+                        <div class="justify-start flex items-center w-full">
+                            <h2 class="card-title text-2xl text-[#c5c5c5] h-14 pl-6 rounded-t-xl">{{ $message->titel  }}</h2>
+                        </div>
+                        <div class="justify-end flex items-center pr-8">
                             <p class="">{{$message->user?->name}}</p>
+                        </div>
+                    </div>
+
+                        <div class="card-body group">
+                        <p class="opacity-0 group-hover:opacity-100 w-full h-full transition ease-in-out duration-500 text-[#c5c5c5]">{{ $message->text }} </p>
+                            <div class="flex w-full justify-between">
+                            <div class="card-actions items-center">
+                                <a href="" class="btn btn-ghost text-[#C5C5C5]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" />
+                                    </svg>
+                                </a>
+                            </div>
+                        <div class="card-actions items-center">
                             <a href="{{route("message.edit",$message->id)}}" class="btn btn-ghost text-[#C5C5C5]">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
